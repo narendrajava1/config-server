0d4b4050c4cef82d8e6d47eed57c840ed8e8d55d
narendrajava1 <narendrakolli08@gmail.com>
2023-06-28 14:36:14 +0530

    intial commit

---
+++ src/test/java/com/naren/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naren.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
